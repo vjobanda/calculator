0732c351315575b0e8f129c1396ef4f84cae0b38
Vivien-Junior Obanda <vjobanda@MacBook-Pro-de-Vivien-Junior-2.local>
2025-03-01 23:15:48 +0100

    Improve the readme file

--- src/calculator.py
+++ src/calculator.py
@@ -2,11 +2,3 @@ class Calculator :
     # return the sum between 2 operands
     def mysum(self, first_operand, second_operand):
         return first_operand + second_operand
-
-    # return the minimum between 2 operands
-    def mymin(self, first_operand, second_operand):
-        return first_operand if second_operand > first_operand else second_operand
-
-    # return the maximum between 2 operands
-    def mymax(self, first_operand, second_operand):
-        return first_operand if second_operand < first_operand else second_operand
--- src/main.py
+++ src/main.py
@@ -4,12 +4,13 @@ from calculator import Calculator
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-op", "--operation", help="(sum, min, max...)")
+    parser.add_argument("-op", "--operation", help="(sum, ...)")
     parser.add_argument("-val1", "--first_value", type=int, help="Give the firt value")
     parser.add_argument("-val2", "--second_value", type=int, help="Give the second value")
 
     args = parser.parse_args()
     calc = Calculator()
+
     if args.operation == 'sum':
         print(f'{args.first_value} + {args.second_value} = {calc.mysum(args.first_value, args.second_value)}')
     else :
