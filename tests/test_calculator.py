31b6a2554e9b0c2d203aa6eb8ef22f12432e053a
Vivien-Junior Obanda <vjobanda@MacBook-Pro-de-Vivien-Junior-2.local>
2025-03-02 01:44:49 +0100

    Correct code smell

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,15 +1,12 @@
 from unittest import TestCase
 from src.calculator import Calculator
 
-class Test_Calculator(TestCase):
+class TestCalculator(TestCase):
     def setUp(self):
         self.calc = Calculator()
 
     def test_sum(self):
         self.assertEqual(self.calc.mysum(1, 2), 3)
-
-    def test_min(self):
-        self.assertEqual(self.calc.mymin(1, 2), 1)
 
     
     
